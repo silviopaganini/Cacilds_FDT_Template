f52d70de7d2e70acfcdbd2b31368741a5c9501d6
Silvio Paganini <s2paganini@gmail.com>
2011-08-18 16:23:38 +0100

    version 0.2

---
+++ Cacilds/project/bin/js/main.js
@@ -0,0 +1,28 @@
+var swfVersionStr = "10.1.0";
+var xiSwfUrlStr = "swf/playerProductInstall.swf";
+
+var flashvars = {
+		xmlPath   : "data/content.xml",
+		cssPath   : "data/styles.css",
+		fontsPath : "swf/font.swf"
+};
+
+var params = {
+    quality             : "best";
+    bgcolor             : "#ffffff";
+    allowscriptaccess   : "sameDomain";
+    allowfullscreen     : "true";
+};
+
+var attributes = {
+    id      : "Main";
+    name    : "Main";
+    align   : "middle";
+};
+swfobject.embedSWF(
+    "swf/${projectNameBuild}.swf", "flashContent", 
+    "${htmlWidth}", "${htmlHeight}", 
+    swfVersionStr, xiSwfUrlStr, 
+    flashvars, params, attributes);
+
+swfobject.createCSS("#flashContent", "display:block;text-align:left;");
